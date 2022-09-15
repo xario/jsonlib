77576fe20024a7c0836870057e6bf9058932ae11
akh <git@xar.io>
2022-09-15 16:27:57 +0200

    updated version

--- jsonlib.h
+++ jsonlib.h
@@ -3,13 +3,7 @@
 
 #include <Arduino.h>
 
-// remove all white space from the json string... preserving strings
-String jsonRemoveWhiteSpace(const String& json);
-
-// index a json list
 String jsonIndexList(String json, int idx);
-
-// extract a json component from json
-String jsonExtract(const String& json, const String& name);
+String jsonExtract(String json, String name);
 
 #endif
